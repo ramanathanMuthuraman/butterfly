2f37c8eb0290b1300b3a58b52c7b44dec8d3d825
ramanathanMuthuraman <ramanathan.gksvv@gmail.com>
2015-01-01 19:38:35 +0530

    initial push

--- script/app.js
+++ script/app.js
@@ -7,7 +7,7 @@ wings.to(".right .top",1,{rotationX:"-50deg",top:-4})
 .to(".right .bottom",1,{rotationX:"-50deg",top:"-24"},0)
 .to(".left .bottom",1,{rotationX:"-50deg",top:"-16"},0);
 
-var butterfly = new TimelineMax({});
+var butterfly = new TimelineMax();
 butterfly.to(".butterfly",5,{left:$(".canvas").width()},"end")
 .to(".butterfly",0.5,{opacity:0},"end+=5");
 
